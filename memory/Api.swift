e4329f6f879eb052f8ae110d987ca87db6d45bff
EchoMeng <mengxiangxxa@163.com>
2018-06-03 15:58:17 +0800

    调整

--- memory/Api.swift
+++ memory/Api.swift
@@ -53,7 +53,7 @@ public class Api {
         } else if type == 4 {
             return #imageLiteral(resourceName: "pengyou")
         } else {
-            return #imageLiteral(resourceName: "qita")
+            return #imageLiteral(resourceName: "4911528005124_.pic_hd.jpg")
         }
     }
     
